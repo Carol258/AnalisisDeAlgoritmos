6b2c23f81ddb6ba463741f39778c05ccc4ff00bc
Carol258 <43769777+Carol258@users.noreply.github.com>
2021-12-14 17:40:34 -0600

    Codeforces round #758

---
+++ A - Find Array.c
@@ -0,0 +1,27 @@
+
+
+    #include <stdio.h>
+     
+    int main(void){
+     
+        int sizeA;
+     
+        scanf("%i", &sizeA);
+     
+        int age[sizeA];
+     
+        for(int i=0; i<100; i++){
+     
+            scanf("%i", &age[i]);
+     
+        }
+     
+        for(int i=0; i<100; i++){
+     
+            scanf("%i", &age[i]);
+     
+        }
+     
+        return 0;
+     
+    }
